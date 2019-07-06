6d96efbece89ed2a4ed72a970f8f7e260fc714ec
Your Name <fikiirmansyah@gmail.com>
2019-07-06 18:25:54 +0700

    initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/fiki/listview/Hero.java
@@ -0,0 +1,31 @@
+package com.dicoding.picodiploma.fiki.listview;
+
+public class Hero {
+    private int foto;
+    private String name;
+    private String Description;
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
